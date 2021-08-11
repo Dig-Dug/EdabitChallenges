77f3f8e6b393e3cd0de24a69739a1093f303f70f
Dig-Dug <40063087+Dig-Dug@users.noreply.github.com>
2021-08-11 12:27:50 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+function calcAge(age) {
+   //A// const calcAge = age => age * 365;
+  //B  return age * 365;
+  /*C  let ageInDays = (age*365)>=0 && typeof(age*365) == 'number' ? (age*365) : []
+	   return ageInDays*/
+}
+   
+   
+  
+   
