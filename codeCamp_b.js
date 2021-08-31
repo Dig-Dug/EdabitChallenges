4b3d25d0015631e350e6c2ffd135bb157c741d57
unknown <dig-dug>
2021-08-31 15:28:34 +0200

    comitting

--- codeCamp_b.js
+++ codeCamp_b.js
@@ -1,2 +1,139 @@
 /***************Golf Code********************** */
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  switch(par, strokes){
+case(1):
+ return names[0];
+case(4,2):
+return names[1];
+case(par-1):
+return names[2];
+case(par):
+return names[3];
+case(par + 1):
+return names[4];
+case(par + 2):
+return names[5];
+case( par + 3):
+return names[6];
+  }
+return names[6];
+  // Only change code above this line
+}
 
+golfScore(5, 4);
+
+/******Selecting from Many Options with Switch Statements********** */
+/**and default */
+
+function caseInSwitch(val) {
+    var answer = "";
+    // Only change code below this line
+    switch(val){
+        case 1: answer = "alpha";break;
+        case 2: answer = "beta";break;
+        case 3: answer = "gamma";break;
+        case 4: answer = "delta"
+    }
+  // Only change code above this line
+    return answer;
+  }
+  
+  caseInSwitch(1);
+
+  /**function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
+ switch(val){
+        case "a": answer = "apple";break;
+        case "b": answer = "bird";break;
+        case "c": answer = "cat";break;
+        default: answer = "stuff";
+    }
+// Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1); */
+
+
+/********Multiple Identical Options in Switch Statements */
+
+/**
+ * function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+switch(val){
+     case 1: case 2: case 3:
+     answer = "Low"; break; 
+     case 4: case 5: case 6:
+     answer = "Mid"; break;
+     case 7: case 8: case 9:
+     answer = "High"; break;   
+        }
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+ */
+
+/***Replacing If Else Chains with Switch** */
+function chainToSwitch(val) {
+    var answer = "";
+    // Only change code below this line
+     switch(val){
+            case "bob": answer = "Marley";  break;
+             case  42: answer = "The Answer";  break;
+             case  1: answer = "There is no #1";  break;
+             case  99: answer = "Missed me by this much!"; break; 
+             case  7: answer = "Ate Nine";  break; 
+       default: answer = "";
+     }
+     
+
+   /* if (val === "bob") {
+        answer = "Marley";
+      } else if (val === 42) {
+        answer = "The Answer";
+      } else if (val === 1) {
+        answer = "There is no #1";
+    } else if (val === 99) {
+        answer = "Missed me by this much!";
+      } else if (val === 7) {
+        answer = "Ate Nine";
+      }*/
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);
+
+
+  /*************Returning Boolean Values from Functions */
+                      /**instead of if else  */
+
+  function isLess(a, b) {
+    return a < b;
+  }
+  
+  isLess(10, 15);
+
+
+  /****Return Early Pattern for Functionsoooooooooooooooooooo */
+ 
+myFun();
+// Setup
+    function abTest(a, b) {
+    // Only change code below this line
+        if(a < 0 || b < 0){
+            return undefined;
+        }
+    // Only change code above this line
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  abTest(2,2);
+
+  /********************Counting Cards*************** */
+  
