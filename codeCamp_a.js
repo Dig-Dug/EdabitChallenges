8c09c9e69c471839cdf1c5dd61baff138445fb25
unknown <dig-dug>
2021-08-31 12:41:49 +0200

    comitting

--- codeCamp_a.js
+++ codeCamp_a.js
@@ -212,14 +212,30 @@ testElseIf(7);
 /**Logical Order in If Else Statements;;;;;;;;;;;;;;;;;;;;;;; */
 
 function orderMyLogic(val) {
-  if (val < 10) {
-    console.log("swwww")
-    return "Less than 10";
-  } else if (val < 5) {
+  if (val < 5) {
     return "Less than 5";
+    
+  } else if (val < 10) {
+    return "Less than 10";
   } else {
     return "Greater than or equal to 10";
   }
 }
 
 orderMyLogic(7);
+
+/*******Chaining If Else Statements************ */
+function testSize(num) {
+  // Only change code below this line
+if (num < 5){return "Tiny"}
+else if(num < 10){return "Small"}
+else if(num < 15){return "Medium"}
+else if(num < 20){return "Large"}
+else if(num >= 20){return "Huge"}
+
+return "Change Me";
+  // Only change code above this line
+}
+
+testSize(7);
+
