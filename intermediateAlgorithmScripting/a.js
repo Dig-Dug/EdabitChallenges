9c845a915dafe63eb3e04e8c72f5cdfd63a6cb8b
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-06-13 16:47:29 +0200

    -

--- intermediateAlgorithmScripting/a.js
+++ intermediateAlgorithmScripting/a.js
@@ -9,39 +9,25 @@ Hint 3
 Remember to that you must add all the numbers in 
 between so this would require a way to get those numbers. */  
 
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-/**
- * 
-{const array1 = [1, 2, 3, 4];
-const reducer = (previousValue, currentValue) => 
-                previousValue + currentValue;
 
-// 1 + 2 + 3 + 4
-console.log(array1.reduce(reducer));} 
-console.log(array1.reduce(reducer, 5));
- */
 function sumAll(arr) {
-    let min = Math.min(...arr);
-      let max = Math.max(...arr);
-      //let total = min + max;
-      let copy = [...arr]; 
-      let i = 0;let j = 0;
-      for(i in arr){
-        let a = min++;
-        let b = max--;
-        let total = (a , b) => a + b;
-        let totall = arr.reduce(total);
-     //  /*  for(j in arr){
-     //   console.log(arr[i,j])
-      //  } 
-        //console.log(a,b);
-        console.log(totall);
-       // return totall;
-      }
-      let med = "?";
-      //console.log(min, max)
-      return 1;
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  //let total = min + max;
+  let copy = [...arr];
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    while (min < max) {
+      j += min++
+      console.log(j + max) //
     }
-    
-    sumAll([1, 4]);
+}
+  return j + max;
+}
+ 
+ sumAll([1, 4]);  // 10
+ sumAll([5,10]);  //45
 
+
+
+ //Diff Two Arrays------------------------------------------------------
