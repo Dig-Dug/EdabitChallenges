8cfacf19d6a22afe42f96566ac44a759785b5428
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-06-14 11:01:32 +0200

    -

--- intermediateAlgorithmScripting/a.js
+++ intermediateAlgorithmScripting/a.js
@@ -31,3 +31,78 @@ function sumAll(arr) {
 
 
  //Diff Two Arrays------------------------------------------------------
+ /**Hint 1
+Merge the list to make it easy to compare functions.
+
+Hint 2
+Use filter to get the new array, you will need to create 
+a callback function.
+
+Hint 3
+The best way to go about the callback function is
+ to check if the number from the new merged array
+  is not in both original arrays and return it. */
+ function diffArray(arr1, arr2){
+  const newArr = [];
+  return newArr;
+ }
+
+ diffArray([1,2,3,5],[1,2,3,4,5]);
+
+
+ /**
+function toFindDuplicates(arry) {
+    const uniqueElements = new Set(arry);
+    const filteredElements = arry.filter(item => {
+        if (uniqueElements.has(item)) {
+            uniqueElements.delete(item);
+        } else {
+            return item;
+        }
+    });
+
+    return [...new Set(uniqueElements)]
+} */
+/**
+ * 
+toFindDuplicates();
+function toFindDuplicates() {
+    let arry = [1, 2, 1, 3, 4, 3, 5];
+    let resultToReturn = false;
+    // call some function with callback function as argument
+    resultToReturn = arry.some((element, index) => {
+        return arry.indexOf(element) !== index
+    });
+    if (resultToReturn) {
+        console.log('Duplicate elements exist'
+            ');
+        }
+        else {
+            console.log('Duplicates don'
+                t exist ');
+            }
+        }
+ */
+/**
+ * 
+array1 = array1.filter(val => !array2.includes(val));
+ */
+
+/** MEEEEEEEEEEEEEEEEEEEE
+function diffArray(arr1, arr2) {
+const newArr = arr1.concat(arr2);
+const newi = new Set(newArr);
+let or;
+const fil = newArr.filter(i => {
+  if(newi.has(i)){
+    or = i;
+    console.log(or)
+    return or;
+  }else {
+    newi.delete(i);
+ }
+});
+ console.log(fil);
+ return fil
+}
+ */
