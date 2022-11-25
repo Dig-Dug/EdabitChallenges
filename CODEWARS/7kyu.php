df83eef9d64b338571f7fe58d14f3802c80c533c
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-11-25 21:51:21 +0100

    php

---
+++ CODEWARS/7kyu.php
@@ -0,0 +1,15 @@
+//Vowel Count Return the number (count) of vowels in the given string.
+
+function getCount($str) {
+  $vowelsCount = 0;
+  // enter your magic here
+  preg_match_all('/[aeiou]/i', $str,$vowelsCount);
+  return count($vowelsCount[0]);
+}
+
+//return every word to uppercase
+function toJadenCase($e) 
+{
+  echo(ucwords($e));
+  return ucwords($e);
+}
