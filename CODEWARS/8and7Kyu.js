f0ad90b167ef5f70c04cafc2017121a1c1901473
unknown <40063087+Dig-Dug@users.noreply.github.com>
2023-02-14 15:37:36 +0100

    2023 New Year

--- CODEWARS/8and7Kyu.js
+++ CODEWARS/8and7Kyu.js
@@ -817,3 +817,7 @@ function howManyLightsabersDoYouOwn(name) {
 function shortcut(string) {
     return string.replace(/[aeiou]/gi, '') //no aeiou
 }
+//Is n divisible by x and y?
+function isDivisible(n, x, y) {
+    return n % x == 0 && n % y == 0 ? true : false
+}
