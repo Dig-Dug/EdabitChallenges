fe9258f3c18304798767595e171b4b445fdfc25d
felipe loewenstein <comandodada@hotmail.com>
2023-09-01 12:53:05 +0200

    -

--- CODEWARS/8number2.js
+++ CODEWARS/8number2.js
@@ -674,3 +674,26 @@ function removeEveryOther(arr) {
     }
     return newArr
 }
+
+
+function greet(language) {
+	console.log(language);
+
+  return language english ?'Welcome':
+czech?'Vitejte':
+danish?'Velkomst':
+dutch ?'Welkom':
+estonian ?'Tere tulemast':
+finnish? 'Tervetuloa':
+flemish? 'Welgekomen':
+french? 'Bienvenue':
+german? 'Willkommen':
+irish? 'Failte':
+italian? 'Benvenuto':
+latvian? 'Gaidits':
+lithuanian? 'Laukiamas':
+polish? 'Witamy':
+spanish?'Bienvenido':
+swedish? 'Valkommen':
+welsh? 'Croeso':"";
+}
