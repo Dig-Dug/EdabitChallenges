7422dc78087f896d148e9d94ea9baf20f5e009e0
felipe loewenstein <comandodada@hotmail.com>
2023-09-12 22:38:40 +0200

    .

--- CODEWARS/8number2.js
+++ CODEWARS/8number2.js
@@ -675,25 +675,90 @@ function removeEveryOther(arr) {
     return newArr
 }
 
-
 function greet(language) {
-	console.log(language);
+    console.log(language)
 
-  return language english ?'Welcome':
-czech?'Vitejte':
-danish?'Velkomst':
-dutch ?'Welkom':
-estonian ?'Tere tulemast':
-finnish? 'Tervetuloa':
-flemish? 'Welgekomen':
-french? 'Bienvenue':
-german? 'Willkommen':
-irish? 'Failte':
-italian? 'Benvenuto':
-latvian? 'Gaidits':
-lithuanian? 'Laukiamas':
-polish? 'Witamy':
-spanish?'Bienvenido':
-swedish? 'Valkommen':
-welsh? 'Croeso':"";
+    return language == english
+        ? 'Welcome'
+        : czech
+        ? 'Vitejte'
+        : danish
+        ? 'Velkomst'
+        : dutch
+        ? 'Welkom'
+        : estonian
+        ? 'Tere tulemast'
+        : finnish
+        ? 'Tervetuloa'
+        : flemish
+        ? 'Welgekomen'
+        : french
+        ? 'Bienvenue'
+        : german
+        ? 'Willkommen'
+        : irish
+        ? 'Failte'
+        : italian
+        ? 'Benvenuto'
+        : latvian
+        ? 'Gaidits'
+        : lithuanian
+        ? 'Laukiamas'
+        : polish
+        ? 'Witamy'
+        : spanish
+        ? 'Bienvenido'
+        : swedish
+        ? 'Valkommen'
+        : welsh
+        ? 'Croeso'
+        : ''
+}
+
+//Get planet by id
+function getPlanetName(id) {
+    var name
+    switch (id) {
+        case 1:
+            name = 'Mercury'
+            break
+        case 2:
+            name = 'Venus'
+            break
+        case 3:
+            name = 'Earth'
+            break
+        case 4:
+            name = 'Mars'
+            break
+        case 5:
+            name = 'Jupiter'
+            break
+        case 6:
+            name = 'Saturn'
+            break
+        case 7:
+            name = 'Uranus'
+            break
+        case 8:
+            name = 'Neptune'
+            break
+    }
+
+    return name
+}
+//Sort and Star
+function twoSort(s) {
+    let r = s.sort()
+    let rr = r[0]
+    let rrr = []
+    let rrrr
+    for (i in rr) {
+        rrr.push(rr[i] + '***')
+        console.log(rrr)
+        i++
+    }
+    rrrr = rrr.join('')
+    rrrr = rrrr.substring(0, rrrr.length - 3)
+    return rrrr //b***i***t***c***o****i***n
 }
