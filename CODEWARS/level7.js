b1924e91506d16ee80171a2d04c33dc6c3127538
felipe loewenstein <comandodada@hotmail.com>
2023-10-24 11:33:19 +0200

    bubbles

--- CODEWARS/level7.js
+++ CODEWARS/level7.js
@@ -2,3 +2,21 @@
 let digits = (n) => {
     return n.toString().split('').length
 }
+
+//Bubblesort Once
+
+function bubblesortOnce(a) {
+    let aa = [...a]
+    let swap
+    for (let j = 0; j < a.length; j++) {
+        for (let i = 0; i < a.length; i++) {
+            if (aa[i] > aa[i + 1]) {
+                swap = aa[i]
+                aa[i] = aa[i + 1]
+                aa[i + 1] = swap
+            }
+        }
+        return aa
+    }
+    return []
+}
