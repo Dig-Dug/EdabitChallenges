4ec1b2e0b23fe69b8b22cf0c788d933dafa70083
felipe loewenstein <comandodada@hotmail.com>
2024-04-12 12:30:05 +0200

    back to php

---
+++ CODEWARS/8.php
@@ -0,0 +1,54 @@
+<?php
+//join strings from array
+function smash(array $words): string
+{
+    return join(" ", $words);
+}
+//sum elements in array
+function sum(array $a): float
+{
+    return array_sum($a);
+}
+//Replace vowels in string with something else
+function replace(string $s): string
+{
+    $vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
+    return $onlyExclamation = str_replace($vowels, "!", $s);
+}
+
+function replaceOther(string $s): string
+{
+    return preg_replace('/[aeiou]/i', "!", $s);
+}
+
+function remove_exclamation_marks($string)
+{
+    return preg_replace('/[!]/i', "", $string);
+}
+//Even or Odd
+
+function even_or_odd(int $n): string
+{
+    return $n % 2 == 0 ? "Even" : "Odd";
+}
+//remove min and max, then sum array
+function sumArray($array)
+{
+    /*$min = min($array);
+     $max = max($array);*/
+    sort($array);
+    array_shift($array);
+    array_pop($array);
+    $sum = array_sum($array);
+    $arrlength = count($array);
+    for ($x = 0; $x < $arrlength; $x++) {
+        echo $array[$x];
+        echo "<br>";
+    }
+    return $sum;
+}
+
+function sumArrayBetter($array)
+{
+    return array_sum($array) - max($array) - min($array);
+}
